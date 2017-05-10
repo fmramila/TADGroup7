51b7f06a140fb57c2de42048bcfe39befe528c3b
sorul <cromerovargas2d@gmail.com>
2017-05-10 15:33:57 +0200

    .

--- CCAmistadesLargas/src/main/java/tad/grupo7/ccamistadeslargas/DAO/ParticipanteDAO.java
+++ CCAmistadesLargas/src/main/java/tad/grupo7/ccamistadeslargas/DAO/ParticipanteDAO.java
@@ -25,6 +25,11 @@ public class ParticipanteDAO {
     private static DB dataBase = new MongoClient("localhost", 27017).getDB("CC");
     private static DBCollection participantes = dataBase.getCollection("Participante");
 
+    /**
+     * Crear en la BD un participante.
+     * @param nombre String nombre del participante.
+     * @param idAmigoDe ObjectId del usuario creador del participante.
+     */
     public static void create(String nombre, ObjectId idAmigoDe) {
         BasicDBObject document = new BasicDBObject();
         document.append("nombre", nombre);
@@ -32,6 +37,11 @@ public class ParticipanteDAO {
         participantes.insert(document);
     }
 
+    /**
+     * Actualizar el participante en la BD.
+     * @param id ObjectId del participante a actualizar.
+     * @param nombre String nombre nuevo del participante.
+     */
     public static void update(ObjectId id, String nombre) {
         BasicDBObject newParticipante = new BasicDBObject();
         newParticipante.append("$set", new BasicDBObject().append("nombre", nombre));
@@ -39,10 +49,19 @@ public class ParticipanteDAO {
         participantes.update(oldParticipante, newParticipante);
     }
 
+    /**
+     * Elimina un participante de la BD.
+     * @param id ObjectId del participante a eliminar.
+     */
     public static void delete(ObjectId id) {
         participantes.remove(new BasicDBObject().append("_id", id));
     }
 
+    /**
+     * Obtiene un Participante de la BD.
+     * @param id ObjectId del participante a obtener.
+     * @return Participante
+     */
     public static Participante read(ObjectId id) {
         BasicDBObject whereQuery = new BasicDBObject();
         whereQuery.put("_id", id);
@@ -52,6 +71,11 @@ public class ParticipanteDAO {
         return new Participante(id, nombre, idAmigoDe);
     }
     
+    /**
+     * Devuelve un participante de la BD.
+     * @param nombre String nombre del participante.
+     * @return Participante.
+     */
     public static Participante read(String nombre){
         BasicDBObject whereQuery = new BasicDBObject();
         whereQuery.put("nombre", nombre);
@@ -59,6 +83,11 @@ public class ParticipanteDAO {
         return new Participante(document.getObjectId("_id"), nombre, document.getObjectId("idAmigoDe"));
     }
 
+    /**
+     * Obtiene una lista de todos los participantes de un evento.
+     * @param idEvento ObjectId del evento.
+     * @return List<Participante>
+     */
     public static List<Participante> readAllFromEvento(ObjectId idEvento) {
         List<Participante> participantes = new ArrayList<>();
         try {
@@ -77,6 +106,11 @@ public class ParticipanteDAO {
         return participantes;
     }
 
+    /**
+     * Obtiene todos los participantes(amigos) de un Usuario.
+     * @param idUsuario ObjectId del usuario.
+     * @return 
+     */
     public static List<Participante> readAllFromUsuario(ObjectId idUsuario) {
         BasicDBObject whereQuery = new BasicDBObject();
         whereQuery.put("_id", idUsuario);
@@ -96,6 +130,11 @@ public class ParticipanteDAO {
         return participantes;
     }
 
+    /**
+     * Obtiene un listado de todos los deudores de un pago de un evento.
+     * @param idGasto ObjectId del gasto.
+     * @return List<Participante>
+     */
     public static List<Participante> readAllDeudoresFromPago(ObjectId idGasto) {
         BasicDBObject whereQuery = new BasicDBObject();
         whereQuery.put("_id", idGasto);
@@ -110,6 +149,12 @@ public class ParticipanteDAO {
         return participantes;
     }
     
+    /**
+     * Obtiene un BasicDBObject de la BD.
+     * @param nombre String nombre del participante.
+     * @param idAmigoDe ObjectId del usuario creador del participante.
+     * @return Participante
+     */
     public static BasicDBObject readDBObject(String nombre, ObjectId idAmigoDe) {
         BasicDBObject andQuery = new BasicDBObject();
         List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
@@ -118,6 +163,12 @@ public class ParticipanteDAO {
         andQuery.put("$and", obj);
         return (BasicDBObject) participantes.findOne(andQuery);
     }
+    
+    /**
+     * Obtener un BasicDBObject de la BD.
+     * @param id ObjectId del participante.
+     * @return BasicDBObject
+     */
     public static BasicDBObject readDBObject(ObjectId id) {
         BasicDBObject whereQuery = new BasicDBObject();
         whereQuery.put("_id", id);
